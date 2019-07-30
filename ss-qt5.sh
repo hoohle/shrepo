b60d65911623bd7b6b40fb338ebfd4b40a1f7ff6
master <1608350027@qq.com>
2019-07-30 20:46:53 +0800

    modife again

--- ss-qt5.sh
+++ ss-qt5.sh
@@ -4,7 +4,7 @@ echo "this is just for ubuntu 16.04 and 18.04"
 #sudo apt-add-repository 'ppa:hzwhuang/ss-qt5' &&
 #sudo mv /etc/apt/sources.list.d/hzwhuang-ubuntu-ss-qt5-bionic.list /etc/apt/sources.list.d/hzwhuang-ubuntu-ss-qt5-xenial.list&&
 
-cat > /etc/apt/sources.list.d/hzwhuang-ubuntu-ss-qt5-xenial.list <<EOF
+sudo cat > /etc/apt/sources.list.d/hzwhuang-ubuntu-ss-qt5-xenial.list <<EOF
 deb http://ppa.launchpad.net/hzwhuang/ss-qt5/ubuntu xenial main
 deb-src http://ppa.launchpad.net/hzwhuang/ss-qt5/ubuntu xenial main
 EOF
